3c568ab4552d22535448ac3fc8dae31de62ca4ed
Gustavo Oli <69ggustavoo69@gmail.com>
2024-02-03 14:13:57 -0300

    second commit

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,33 @@
-let cont = 0;
+let textAreaCripto = document.getElementById("criptografar-texarea");
+let textAreaDescripto = document.getElementById("descriptografar-texarea");
+var robotAnim = document.getElementById("robot");
+
+function playAnim(){
+    robotAnim.style.animation = "robotJump 2s";
+    setTimeout(function() {
+        robotAnim.style.animation = "";
+    }, 950);
+}
 
 function clickButtonCripto() {
- document.getElementById("robot").style.animation = "robotJump 2s 1";
+    playAnim();
+ var txtValue = textAreaCripto.value;
+ txtValue = txtValue.replace(/e/g, "long").replace(/i/g, "pouuyn").replace(/a/g, "ober").replace(/u/g, "udumb");
+ textAreaDescripto.value = txtValue;
 
+ 
 }
+
+function clickButtonDescripto() {
+    playAnim();
+    var txtValueDes = textAreaDescripto.value;
+    txtValueDes = txtValueDes.replace(/long/g, "e").replace(/pouuyn/g, "i").replace(/ober/g, "a").replace(/udumb/g, "u");
+    textAreaDescripto.value = txtValueDes;
+  
+}
+function buttonCopy() {
+    var txtCopy = document.getElementById('descriptografar-texarea');
+    txtCopy.select();
+    document.execCommand('copy');
+}
+
